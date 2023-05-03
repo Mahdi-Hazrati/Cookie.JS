15d0d5dbc03249762c6201dc950e171a905ee028
Mehdi <127740921+Mahdi-Hazrati@users.noreply.github.com>
2023-05-03 21:11:20 +0330

    Create cookie.js

---
+++ cookie.js
@@ -0,0 +1,38 @@
+// create new cookie from given atugment
+function setCookie(name, value, path, expiresDay) {
+    if (name && value && path && expiresDay) {
+        let now = new Date();
+        now.setTime(now.getTime() + expiresDay * 24 * 60 * 60 * 1000);
+        let expires = now.toUTCString();
+        document.cookie = `${name}=${value};path=${path};expires=$${expires}`;
+    } else {
+        console.error("missing argument in setCookie() function");
+    }
+}
+
+// get a cookie value from name of cookie
+function getCookie(name) {
+    let cookies = document.cookie.split(";");
+    let cookie = undefined;
+
+    cookies.some((c) => {
+        if (c.includes(name)) {
+            cookie = c.substring(c.indexOf("=") + 1);
+            return true;
+        }
+    });
+
+    return cookie;
+}
+
+// a function for delete cookie from given cookie name
+function delCookie(name) {
+    if (name) {
+        let date = new Date();
+        date.setTime(date.getTime() - 1 * 24 * 60 * 60 * 1000);
+        let expiredDate = date.toUTCString();
+        document.cookie = `${name}=value;path=/;expires=$${expiredDate}`;
+    }
+}
+
+export { setCookie, getCookie, delCookie };
